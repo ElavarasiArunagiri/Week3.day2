ac4185b723c01cbd51a92bd583a7535e139f8909
Elavarasi A <90094173+ElavarasiArunagiri@users.noreply.github.com>
2021-09-24 17:44:19 +0530

    Add files via upload

--- ErailSort.java
+++ ErailSort.java
@@ -21,53 +21,53 @@ public class ErailSort {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("https://erail.in/");
 		driver.findElement(By.xpath("//input[@id ='chkSelectDateOnly']")).click();
-		
+
 		WebElement from = driver.findElement(By.id("txtStationFrom"));
 		from.clear();
 		from.sendKeys("MDU", Keys.ENTER);
-		
+
 		WebElement to = driver.findElement(By.id("txtStationTo"));
 		to.clear();
 		to.sendKeys("PER", Keys.ENTER);
-		//sleep to wait till the results get loaded on page.
+		// sleep to wait till the results get loaded on page.
 		Thread.sleep(2000);
-		List<WebElement> trainName = driver.findElements(By.xpath("//table[@class='DataTable TrainList TrainListHeader']//td[2]"));
+		List<WebElement> trainName = driver
+				.findElements(By.xpath("//table[@class='DataTable TrainList TrainListHeader']//td[2]"));
 		int size = trainName.size();
-		System.out.println("The no. of trains listed: "+size);
-		
-		//Converting WebElement list to String List in order to use in sort method.
+		System.out.println("The no. of trains listed: " + size);
+
+		// Converting WebElement list to String List in order to use in sort method.
 		ArrayList<String> strTrainName = new ArrayList<String>();
 		// adding the train name to a string list.
-		for(int i=0; i<size; i++)
-		{
+		for (int i = 0; i < size; i++) {
 			String tname = trainName.get(i).getText();
 			strTrainName.add(tname);
-			
+
 		}
-		//for each to get the text of the webelement throws Stale Element reference exception but for loop works.
-		/*for(WebElement wTN : trainName) {
-			Thread.sleep(2000);
-			String tname = wTN.getText();
-			strTrainName.add(tname);
-			System.out.println(i++ +". "+strTrainName);
-		}*/
-		
-		//printing the unsorted list.
-		int a=1;
+		// for each to get the text of the webelement throws Stale Element reference
+		// exception but for loop works.
+		/*
+		 * for(WebElement wTN : trainName) { Thread.sleep(2000); String tname =
+		 * wTN.getText(); strTrainName.add(tname); System.out.println(i++
+		 * +". "+strTrainName); }
+		 */
+
+		// printing the unsorted list.
+		int a = 1;
 		System.out.println("The train name list: ");
-		for(String unsortedTN : strTrainName) {
-			System.out.println(a++ +". "+unsortedTN);
-		}	
-		
-		Collections.sort(strTrainName);
-		
-		//printing the sorted list.
-		int j=1;
-		System.out.println("The sorted train name list: ");
-		for(String sTN : strTrainName) {
-			System.out.println(j++ +". "+sTN);
+		for (String unsortedTN : strTrainName) {
+			System.out.println(a++ + ". " + unsortedTN);
 		}
-		
-	} 
+
+		Collections.sort(strTrainName);
+
+		// printing the sorted list.
+		int j = 1;
+		System.out.println("The sorted train name list: ");
+		for (String sTN : strTrainName) {
+			System.out.println(j++ + ". " + sTN);
+		}
+
+	}
 
 }
